6ae283c33824d2f8be75019c8344a2479efb284a
ameedDevOps <ameed.devops@gmail.com>
2025-12-15 04:11:41 +0530

    update terraform code

--- terraform/nodegroup.tf
+++ terraform/nodegroup.tf
@@ -1,5 +1,5 @@
 resource "aws_eks_node_group" "default" {
-  cluster_name    = aws_eks_cluster.this.name
+  cluster_name    = aws_eks_cluster.eks_cluster.name
   node_group_name = "default-ng"
   node_role_arn   = aws_iam_role.node_role.arn
   subnet_ids      = aws_subnet.private[*].id
